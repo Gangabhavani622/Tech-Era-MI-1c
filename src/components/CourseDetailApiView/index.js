f8e5efc4954b521d656fc6b0dd921ef3bd4f4ba1
Gangabhavani622 <115251057+Gangabhavani622@users.noreply.github.com>
2023-05-22 02:10:48 +0530

    Update index.js

--- src/components/CourseDetailApiView/index.js
+++ src/components/CourseDetailApiView/index.js
@@ -1,6 +1,6 @@
 import {Component} from 'react'
 import {Loader} from 'react-loader-spinner'
-import {Redirect} from 'react-router-dom'
+
 import './index.css'
 
 const apiStatusConst = {
@@ -17,29 +17,27 @@ class CourseDetailApiView extends Component {
     this.getCourseDetailView()
   }
 
-  onClickReloadPage = () => {
-    const {match} = this.props
-    const {params} = match
-    const {id} = params
-    return <Redirect to={`/courses/${id}`} />
-  }
+ 
 
   getCourseDetailView = async () => {
+     this.setState({apiStatus2: apiStatusConst.inProgress})
     const {match} = this.props
     const {params} = match
     const {id} = params
 
-    this.setState({apiStatus2: apiStatusConst.inProgress})
+   
     const url2 = `https://apis.ccbp.in/te/courses/${id}`
     const options = {
       method: 'GET',
     }
 
+   
     const response = await fetch(url2, options)
-
-    const fetchData = await response.json()
+    
+    
 
     if (response.ok) {
+      const fetchData = await response.json()
       const data = fetchData.course_details
       const updatedData = {
         description: data.description,
@@ -60,7 +58,7 @@ class CourseDetailApiView extends Component {
     }
   }
 
-  renderSuccessView = () => {
+  renderCourseSuccessView = () => {
     const {courseDetails} = this.state
     console.log(courseDetails)
     const {name, imageUrl, description} = courseDetails
@@ -78,15 +76,17 @@ class CourseDetailApiView extends Component {
     )
   }
 
-  renderInProgressView = () => <Loader data-testid="loader" />
+  renderCourseInProgressView = () =>(<div data-testid="loader" >
+                                     <Loader />
+                                     </div> )
 
-  renderFailureView = () => (
+  renderCourseFailureView = () => (
     <div className="failure-container">
       <img src="" alt="failure view" className="failure-image" />
       <div>
         <button
           type="button"
-          onClick={this.onClickReloadPage}
+          onClick={this. getCourseDetailView}
           className="retry-btn"
         >
           Retry
@@ -101,11 +101,11 @@ class CourseDetailApiView extends Component {
     console.log(courseDetails)
     switch (apiStatus2) {
       case apiStatusConst.success:
-        return this.renderSuccessView()
+        return this.renderCourseSuccessView()
       case apiStatusConst.inProgress:
-        return this.renderInProgressView()
+        return this.renderCourseInProgressView()
       case apiStatusConst.failure:
-        return this.renderFailureView()
+        return this.renderCourseFailureView()
       default:
         return null
     }
