e9ac7feed0b97435019b71d8909fef74f1a753c9
Gangabhavani622 <115251057+Gangabhavani622@users.noreply.github.com>
2023-05-22 13:53:40 +0530

    Update index.js

--- src/components/CourseDetailApiView/index.js
+++ src/components/CourseDetailApiView/index.js
@@ -1,5 +1,5 @@
 import {Component} from 'react'
-import {Loader} from 'react-loader-spinner'
+import Loader from 'react-loader-spinner'
 
 import './index.css'
 
@@ -77,7 +77,7 @@ class CourseDetailApiView extends Component {
   }
 
   renderCourseInProgressView = () =>(<div data-testid="loader" >
-                                     <Loader />
+                                     <Loader type='ThreeDots' />
                                      </div> )
 
   renderCourseFailureView = () => (
